440c9b2d8030c5e98c165dcd66f0d946084078be
Kamitotomi <rockyaaokage@gmail.com>
2020-06-05 15:10:13 +0900

    to adjust

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	def after_sign_in_path(resource)
+		if current_user
+			flash[:notice] = "Signed in successfully."
+			user_path
+		else
+			flash[:notice] = "Welcome! You have signed up successfully."
+			user_path
+		end
+	end
+  
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
